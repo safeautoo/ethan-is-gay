1bae24c6abc2caa7414f1fbeeeb83511a788e65f
Odd <owenhines07@gmail.com>
2017-02-03 22:51:35 -0600

    Added customisable euthanization

--- src/Main.cpp
+++ src/Main.cpp
@@ -2,13 +2,19 @@
 
 using namespace std;
 
+string input;
+
 int endCareer(string lovedOne) {
   cout << "ur " << lovedOne << " gay" << endl;
 }
 
 int main() {
 
-  endCareer("brother");
+  cout << "Please input your loved one's name to be euthanized." << endl;
+
+  cin >> input;
+
+  endCareer(input);
 
   return 0;
 
